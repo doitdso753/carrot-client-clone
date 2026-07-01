3d480a1e54884fa53eb11aa9da995ead7b6bafae
ParkSoHyeon <dso753@naver.com>
2026-07-01 22:58:24 +0900

    feat: 대시보드 페이지 추가 - 카테고리 아이콘 type 추가

---
+++ src/types/types.ts
@@ -0,0 +1,14 @@
+export type CategoryIconName =
+  | 'shoppingBag'
+  | 'searchUser'
+  | 'home'
+  | 'car'
+  | 'store'
+  | 'document'
+  | 'group'
+  | 'mug';
+
+export type CategoryItem = {
+  label: string;
+  iconName: CategoryIconName;
+};
