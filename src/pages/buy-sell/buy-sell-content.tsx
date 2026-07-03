9d60505bc0189db3361ecc5c4dea98d704f1ba00
ParkSoHyeon <dso753@naver.com>
2026-07-03 11:02:31 +0900

    feat: 중고거래 페이지 추가 - 헤더, 메인 여백 수정

---
+++ src/pages/buy-sell/buy-sell-content.tsx
@@ -0,0 +1,8 @@
+import type { ReactNode } from 'react';
+
+export default function BuySellContent(): ReactNode {
+  return (
+    <main className="buy-sell-wrapper min-h-screen pb-20">
+    </main>
+  );
+}
--- src/pages/buy-sell/index.tsx
+++ src/pages/buy-sell/index.tsx
@@ -1,12 +1,14 @@
 import type { ReactNode } from 'react';
 import RootLayout from '@/layouts/root-layout';
 import HeaderWithSearch from '@/layouts/header-with-search.tsx';
+import BuySellContent from '@/pages/buy-sell/buy-sell-content.tsx';
 
 export default function BuySellPage(): ReactNode {
   return (
     <RootLayout>
       <div className="flex min-h-screen flex-col">
         <HeaderWithSearch />
+        <BuySellContent />
       </div>
     </RootLayout>
   );
