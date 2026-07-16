f146e406d29e252c2ece8d4fec2b0f406e91e352
ParkSoHyeon <dso753@naver.com>
2026-07-17 01:14:11 +0900

    feat: 동네업체 상세 페이지 구현 - 아이콘 추가 - 폴더 정리

---
+++ src/assets/icons/common/copy-icon.tsx
@@ -0,0 +1,28 @@
+export function CopyIcon() {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      width="18"
+      height="18"
+    >
+      <g>
+        <g>
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M2 8.8C2 7.2536 3.2536 6 4.8 6H15.2C16.7464 6 18 7.2536 18 8.8V19.2C18 20.7464 16.7464 22 15.2 22H4.8C3.2536 22 2 20.7464 2 19.2V8.8ZM4.8 8C4.35817 8 4 8.35817 4 8.8V19.2C4 19.6418 4.35817 20 4.8 20H15.2C15.6418 20 16 19.6418 16 19.2V8.8C16 8.35817 15.6418 8 15.2 8H4.8Z"
+            fill="currentColor"
+          ></path>
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M7 3C7 2.44772 7.44772 2 8 2H19.2C20.7464 2 22 3.2536 22 4.8V16C22 16.5523 21.5523 17 21 17C20.4477 17 20 16.5523 20 16V4.8C20 4.35817 19.6418 4 19.2 4H8C7.44772 4 7 3.55228 7 3Z"
+            fill="currentColor"
+          ></path>
+        </g>
+      </g>
+    </svg>
+  );
+}
