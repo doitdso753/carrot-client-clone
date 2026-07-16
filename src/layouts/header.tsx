7eed4f42786581853959d221e2ccc289174867f8
ParkSoHyeon <dso753@naver.com>
2026-07-16 18:43:44 +0900

    fix: 대시보드 헤더에 메뉴바 노출

--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -5,7 +5,9 @@ import HeaderCategoryMobileNav from '@/layouts/header-with-search/nav/mobile/hea
 import { HEADER_CATEGORY_ITEMS } from '@/types/header-category-items.ts';
 
 export default function Header(): ReactNode {
+  const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
   const headerRef = useElementHeightCssVariable<HTMLElement>('--header-height');
+
   return (
     <header
       className="header-wrapper flex items-center justify-between"
@@ -14,6 +16,11 @@ export default function Header(): ReactNode {
       <a className="flex items-center gap-3" href="/" aria-label="당근 홈">
         <LogoIcon />
       </a>
+      <HeaderCategoryMobileNav
+        items={HEADER_CATEGORY_ITEMS}
+        isOpen={isMobileNavOpen}
+        onToggle={() => setIsMobileNavOpen((currentValue) => !currentValue)}
+      />
     </header>
   );
 }
