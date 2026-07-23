6f3adbc5efd76ae3cfe36759e865d51e9665a026
ParkSoHyeon <dso753@naver.com>
2026-07-23 13:45:06 +0900

    rename: 팝업 폴더 추가 및 팝업 이동

--- src/components/local-profile/sections/local-profile-introduction.tsx
+++ src/components/local-profile/sections/local-profile-introduction.tsx
@@ -1,6 +1,6 @@
 import type { ReactNode } from 'react';
 import { VerifiedDocumentIcon } from '@/assets/icons';
-import LocalProfileDocumentPopup from '@/components/local-profile/local-profile-document-popup.tsx';
+import LocalProfileDocumentPopup from '@/components/local-profile/popups/local-profile-document-popup.tsx';
 import usePopup from '@/hooks/use-popup.ts';
 import type { LocalProfileItem } from '@/types/types.ts';
 
