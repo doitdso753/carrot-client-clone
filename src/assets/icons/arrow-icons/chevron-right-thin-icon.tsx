9219dd5332fdf5910af2b2082a49a7ecb406d255
ParkSoHyeon <dso753@naver.com>
2026-07-23 13:30:20 +0900

    feat: 동네업체 상세페이지 구현 - 아이콘 추가

---
+++ src/assets/icons/arrow-icons/chevron-right-thin-icon.tsx
@@ -0,0 +1,22 @@
+import type { ReactNode } from 'react';
+
+export function ChevronRightThinIcon(): ReactNode {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+    >
+      <g>
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M8.27589 2.76052C7.89499 3.16043 7.9104 3.79341 8.31032 4.17431L16.55 12.0222L8.31037 19.8689C7.91042 20.2498 7.89496 20.8827 8.27584 21.2827C8.65671 21.6826 9.28969 21.6981 9.68963 21.3172L18.6896 12.7464C18.8878 12.5576 19 12.2959 19 12.0222C19 11.7486 18.8879 11.4868 18.6897 11.2981L9.68968 2.72608C9.28976 2.34518 8.65679 2.3606 8.27589 2.76052Z"
+          fill="currentColor"
+        ></path>
+      </g>
+    </svg>
+  );
+}
---
+++ src/assets/icons/common/reply-icon.tsx
@@ -0,0 +1,22 @@
+import type { ReactNode } from 'react';
+
+export function ReplyIcon(): ReactNode {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      width="18"
+      height="18"
+    >
+      <g>
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M6 5C6.55228 5 7 5.44772 7 6V16.2C7 16.6418 7.35817 17 7.8 17H12.9H18C18.5523 17 19 17.4477 19 18C19 18.5523 18.5523 19 18 19H7.8C6.2536 19 5 17.7464 5 16.2V6C5 5.44772 5.44772 5 6 5Z"
+          fill="currentColor"
+        ></path>
+      </g>
+    </svg>
+  );
+}
