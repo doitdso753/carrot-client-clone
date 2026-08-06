846a8ec255b47da104ba0023dbb9a79b6b2b67cc
ParkSoHyeon <dso753@naver.com>
2026-08-06 18:49:44 +0900

    feat: 중고차 상세 페이지 구현 - 아이콘 추가

---
+++ src/assets/icons/car-icons/person-icon.tsx
@@ -0,0 +1,16 @@
+export function PersonIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="none"
+      aria-hidden="true"
+    >
+      <path
+        fill="#329BE1"
+        d="M8 2.26a3.34 3.34 0 1 0 0 6.68 3.34 3.34 0 0 0 0-6.68Zm0 7.18c-3.1 0-5.14 1.69-5.14 3.94 0 .2.17.36.36.36h9.57c.2 0 .36-.17.36-.36 0-2.25-2.04-3.94-5.14-3.94h-.02H8Z"
+      ></path>
+    </svg>
+  );
+}
